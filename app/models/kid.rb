3498c8abc13884e73b15a254a89888387c590d89
Jason Ghezzi <jasonghezzi@gmail.com>
2014-04-29 13:30:04 -0400

    fixed model syntax

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,6 +1,6 @@
 class Kid < ActiveRecord::Base
 	belongs_to :parent
-	has_many :sits through :families
+	has_many :sits, through: :families
 	validates :age, numericailty: true
 	validates :gender, :family_id, presence: true
 end
