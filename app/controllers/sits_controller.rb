dede27f5a77a9e86605b126cf18aae45c5bbc845
Jason Ghezzi <jasonghezzi@gmail.com>
2014-04-30 16:12:28 -0400

    built CRUD for sits

---
+++ app/controllers/sits_controller.rb
@@ -0,0 +1,47 @@
+class SitsController < ApplicationController
+	
+	def index
+		@sits = Sit.all
+	end
+
+	def create
+		@new_sit = Sit.new(sit_params)
+		if @new_sit.save
+			redirect_to sits_path
+		else
+			render new_sit_path
+		end
+	end
+
+	def new
+		@new_sit = Sit.new
+	end
+
+	def edit
+		@sit = Sit.find(params[:id])
+	end
+
+	def update
+		@sit = Sit.find(params[:id])
+		if @sit.update_attributes(sit_params)
+		redirect_to sits_path
+		end
+	end
+
+	def show
+
+	end
+
+	def destroy
+		@sit = Sit.find(params[:id])
+		@sit.destroy
+		redirect_to sits_path
+	end
+
+	private
+
+	def sit_params
+		params.require(:sit).permit!
+	end
+
+end
