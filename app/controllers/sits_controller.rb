c68736e4f2e32d2393d15bcd29a0cd62b24b26e1
RyanSnodgrass <res0428@yahoo.com>
2014-04-29 23:18:28 -0400

    added CRUD to sitslist

--- app/controllers/sits_controller.rb
+++ app/controllers/sits_controller.rb
@@ -1,2 +1,38 @@
 class SitsController < ApplicationController
+	def index
+		@sits = Sit.all
+	end
+	def new
+		@new_sit = Sit.new
+	end
+	def create
+		@new_sit = Sit.new(sit_params)
+		if @new_sit.save
+			redirect_to sits_path
+		else
+			redirect_to new_sit_path
+		end
+	end
+	def show
+		@sit = Sit.find(params[:id])
+	end
+	def edit
+		@sit = Sit.find(params[:id])
+	end
+	def update
+		@sit = Sit.find(params[:id])
+		@sit.update_attributes(sit_params)
+		redirect_to sits_path
+	end
+	def destroy
+		@sit = Sit.find(params[:id])
+		@sit.destroy
+		redirect_to sits_path
+	end
+
+	private
+
+	def sit_params
+		params.require(:sit).permit!
+	end
 end
