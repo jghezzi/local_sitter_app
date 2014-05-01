53516969d60081bd8724c0edaec26a07cdd4746c
Jeremy Samples <judesamp@gmail.com>
2014-05-01 17:17:44 -0400

    fixed application layout

---
+++ db/migrate/20140501211452_change_type_of_fields_in_sits.rb
@@ -0,0 +1,6 @@
+# class ChangeTypeOfFieldsInSits < ActiveRecord::Migration
+#   def change
+#     change_column :sits, :start_time, :time
+#     change_column :sits, :end_time, :time
+#   end
+# end
