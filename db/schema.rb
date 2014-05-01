900c24f080e0fc6b4a444d18d354582f32ececfb
Jeremy Samples <judesamp@gmail.com>
2014-05-01 18:22:04 -0400

    a little editing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428205820) do
+ActiveRecord::Schema.define(version: 20140501211452) do
 
   create_table "families", force: true do |t|
     t.string   "name"
