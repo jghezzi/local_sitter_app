5c25102a3fb2b04e41fe712e30dc905f909d94f1
Jason Ghezzi <jasonghezzi@gmail.com>
2014-05-02 16:11:44 -0400

    route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
-  get '/sits/sitter_response/:id/edit' => 'sits#sitter_response'
-  put 'sitter_response/:id' => 'sits#sitter_update'
+  # get '/sits/sitter_response/:id/edit' => 'sits#sitter_response'
+  # put 'sitter_response/:id' => 'sits#sitter_update'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  resources :sits
+  resources :sits do
+    member do
+      get :sitter_response
+      put :sitter_update
+    end
+  end
 
   get "login" => "sessions#new"
   get "logout" => "sessions#destroy"
